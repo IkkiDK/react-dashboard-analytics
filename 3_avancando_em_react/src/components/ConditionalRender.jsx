895d5398896cf3d544392eb2acd4dc42d0326a41
IkkiDK <hique.k@bol.com.br>
2025-02-07 11:13:50 -0300

    Modifiquei ConditionalRender.jsx

--- 3_avancando_em_react/src/components/ConditionalRender.jsx
+++ 3_avancando_em_react/src/components/ConditionalRender.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 const ConditionalRender = () => {
   const [x] = useState(false);
-
+  const [name, setName] = useState("Josnei")
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Agora x é falso</p>}
+      {name === "Henrique" ? (
+        <div>
+          <p>O nome é Henrique.</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado.</p>
+        </div>
+      )}
+      <button onClick={() => setName("Henrique")}>Clique aqui!</button>
     </div>
   );
 };
